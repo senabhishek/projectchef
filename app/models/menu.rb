f3b20c6f8746228e1894ba6dd07d48802fe74461
Abhishek Sen <sen.abhishek@gmail.com>
2013-03-02 11:02:33 -0800

    Menu model fixed - HABTM and sign up button created. Form still doesn't work

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,7 +2,9 @@ class Menu < ActiveRecord::Base
   attr_accessible :offered_price
 
   # Associations
-  has_and_belongs_to_many :appetizers, :entrees, :desserts
+  has_and_belongs_to_many :appetizers
+  has_and_belongs_to_many :entrees
+  has_and_belongs_to_many :desserts
   has_many :eventmenus
   has_many :events, :through => :eventmenus
 end
